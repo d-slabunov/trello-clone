268277f35b48ff4af4ad197c4c5420bbc4e8532a
d-slabunov <d.slabunov.exceedteam@gmail.com>
2019-08-13 17:27:46 +0300

    UserBoardsPage switched to class component

--- src/components/pages/UserBoardsPage.jsx
+++ src/components/pages/UserBoardsPage.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import actions from '../../actions/boardActions';
 import BoardListItem from '../boards/BoardListItem';
 import CreateBoard from '../boards/CreateBoard';
 
-const UserBoardsPage = (props) => {
-  const { boards, token } = props;
-  const [state, setState] = useState({
+class UserBoardsPage extends Component {
+  state = {
     createBoardActive: false,
     status: {
       loading: true,
@@ -20,32 +19,19 @@ const UserBoardsPage = (props) => {
         statusCode: undefined,
       },
     },
-  });
+  }
 
-  const openCreateBoard = (e) => {
-    e.preventDefault();
+  componentDidMount() {
+    const { props } = this;
+    const { token } = props;
 
-    setState({
-      ...state,
-      createBoardActive: true,
-    });
-  };
-
-  const closeCreateBoard = () => {
-    setState({
-      ...state,
-      createBoardActive: false,
-    });
-  };
-
-  if (state.status.loading) {
     props.loadAllBoards(token)
       .then((res) => {
         if (res) {
           if (res.status === 200) {
             const { message } = res.data;
 
-            setState({
+            this.setState(state => ({
               ...state,
               status: {
                 loading: false,
@@ -54,12 +40,23 @@ const UserBoardsPage = (props) => {
                   statusCode: 200,
                 },
               },
-            });
+            }));
           }
+        } else {
+          this.setState(state => ({
+            ...state,
+            status: {
+              loading: false,
+              err: {
+                message: 'No connection with server',
+                statusCode: 500,
+              },
+            },
+          }));
         }
       })
       .catch((err) => {
-        setState({
+        this.setState(state => ({
           ...state,
           status: {
             loading: false,
@@ -68,30 +65,52 @@ const UserBoardsPage = (props) => {
               statusCode: 200,
             },
           },
-        });
+        }));
       });
   }
 
-  return (
-    <div className="container-fluid">
-      <h3 className="text-center text-white mt-3">Boards</h3>
+  // console.log('state.status.loading', state.status.loading);
+  openCreateBoard = (e) => {
+    e.preventDefault();
 
-      <div style={{ maxHeight: `${window.innerHeight - 164}px` }} className="row overflow-auto">
-        {boards.map(board => (
-          <div key={board._id} className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-2">
-            <BoardListItem id={board._id} title={board.title} />
-          </div>
-        ))}
+    this.setState(state => ({
+      ...state,
+      createBoardActive: true,
+    }));
+  };
+
+  closeCreateBoard = () => {
+    this.setState(state => ({
+      ...state,
+      createBoardActive: false,
+    }));
+  };
+
+  render() {
+    const { state } = this;
+    const { boards } = this.props;
+
+    return (
+      <div className="container-fluid">
+        <h3 className="text-center text-white mt-3">Boards</h3>
+
+        <div style={{ maxHeight: `${window.innerHeight - 164}px` }} className="row overflow-auto">
+          {boards.map(board => (
+            <div key={board._id} className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-2">
+              <BoardListItem id={board._id} title={board.title} />
+            </div>
+          ))}
+        </div>
+
+        <div className="col-12 px-0 text-center py-3">
+          <a className="text-white" onClick={this.openCreateBoard} href="/">Create a new board</a>
+        </div>
+
+        {state.createBoardActive && <CreateBoard close={this.closeCreateBoard} />}
+
       </div>
-
-      <div className="col-12 px-0 text-center py-3">
-        <a className="text-white" onClick={openCreateBoard} href="/">Create a new board</a>
-      </div>
-
-      {state.createBoardActive && <CreateBoard close={closeCreateBoard} />}
-
-    </div>
-  );
+    );
+  }
 };
 
 const mapStateToProps = state => ({
