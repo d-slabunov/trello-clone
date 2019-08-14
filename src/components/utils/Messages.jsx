582f7d93a47d2a4aa11d3f905aee9d49ea762353
d-slabunov <d.slabunov.exceedteam@gmail.com>
2019-08-14 16:48:15 +0300

    Message components refactored

--- src/components/utils/Messages.jsx
+++ src/components/utils/Messages.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import '../../styles/messages.sass';
 
+const MessageContaner = (props) => {
+  const { children, styles } = props;
+  return (
+    <>
+      <div className="message-container">
+        <div style={styles} className="message message-error">
+          {children};
+        </div>
+      </div>
+      <div className="position-absolute w-100 h-100 bg-white loader-bg" />
+    </>
+  );
+};
+
 const ErrorMessage = (props) => {
   const {
     message,
@@ -11,16 +25,11 @@ const ErrorMessage = (props) => {
     clearInputs,
   } = props;
   return (
-    <>
-      <div className="message-container">
-        <div style={styles} className="message message-error">
-          <h4 className="bg-danger">Error</h4>
-          <h5 className={`${loadingTextAnimation && 'loading-text'} mt-4 w-100`}>{message}</h5>
-          {btn !== false && <button onClick={() => closeMessage(clearInputs)} onKeyPress={() => closeMessage(clearInputs)} type="button" className="btn btn-danger bg-danger my-3">OK</button>}
-        </div>
-      </div>
-      <div className="position-absolute w-100 h-100 bg-white loader-bg" />
-    </>
+    <MessageContaner style={styles}>
+      <h4 className="bg-danger">Error</h4>
+      <h5 className={`${loadingTextAnimation && 'loading-text'} mt-4 w-100`}>{message}</h5>
+      {btn !== false && <button onClick={() => closeMessage(clearInputs)} onKeyPress={() => closeMessage(clearInputs)} type="button" className="btn btn-danger bg-danger my-3">OK</button>}
+    </MessageContaner>
   );
 };
 
@@ -34,16 +43,11 @@ const InfoMessage = (props) => {
     clearInputs,
   } = props;
   return (
-    <>
-      <div className="message-container">
-        <div style={styles} className="message message-info">
-          <h4 className="bg-primary">Info</h4>
-          <h5 className={`${loadingTextAnimation && 'loading-text'} mt-4 w-100`}>{message}</h5>
-          {btn !== false && <button onClick={() => closeMessage(clearInputs)} onKeyPress={() => closeMessage(clearInputs)} type="button" className="btn btn-primary bg-primary my-3">OK</button>}
-        </div>
-      </div>
-      <div className="position-absolute w-100 h-100 bg-white loader-bg" />
-    </>
+    <MessageContaner style={styles}>
+      <h4 className="bg-primary">Info</h4>
+      <h5 className={`${loadingTextAnimation && 'loading-text'} mt-4 w-100`}>{message}</h5>
+      {btn !== false && <button onClick={() => closeMessage(clearInputs)} onKeyPress={() => closeMessage(clearInputs)} type="button" className="btn btn-primary bg-primary my-3">OK</button>}
+    </MessageContaner>
   );
 };
 
@@ -57,16 +61,11 @@ const SuccessMessage = (props) => {
     clearInputs,
   } = props;
   return (
-    <>
-      <div className="message-container">
-        <div style={styles} className="message message-success">
-          <h4 className="bg-success">Success</h4>
-          <h5 className={`${loadingTextAnimation && 'loading-text'} mt-4 w-100`}>{message}</h5>
-          {btn !== false && <button onClick={() => closeMessage(clearInputs)} onKeyPress={() => closeMessage(clearInputs)} type="button" className="btn btn-success bg-success my-3">OK</button>}
-        </div>
-      </div>
-      <div className="position-absolute w-100 h-100 bg-white loader-bg" />
-    </>
+    <MessageContaner style={styles}>
+      <h4 className="bg-success">Success</h4>
+      <h5 className={`${loadingTextAnimation && 'loading-text'} mt-4 w-100`}>{message}</h5>
+      {btn !== false && <button onClick={() => closeMessage(clearInputs)} onKeyPress={() => closeMessage(clearInputs)} type="button" className="btn btn-success bg-success my-3">OK</button>}
+    </MessageContaner>
   );
 };
 
